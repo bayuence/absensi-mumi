13a716acfac4cb4225c5161eb28561cc837ed6b7
bayuence <bayuence@gmail.com>
2025-10-28 03:34:56 +0700

    encee\1234

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,43 +1,54 @@
-    export type Json =
-    | string
-    | number
-    | boolean
-    | null
-    | { [key: string]: Json }
-    | Json[]
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
 
-    export interface Database {
-    public: {
-        Tables: {
-        presensi: {
-            Row: {
-            id: string
-            username: string
-            nama: string
-            tanggal: string
-            waktu: string
-            status_presensi: string
-            }
-            Insert: {
-            id: string
-            username: string
-            nama: string
-            tanggal: string
-            waktu: string
-            status_presensi: string
-            }
-            Update: {
-            id?: string
-            username?: string
-            nama?: string
-            tanggal?: string
-            waktu?: string
-            status_presensi?: string
-            }
+export interface User {
+  id?: string;
+  username: string;
+  nama: string;
+  asal: string;
+  status: string;
+  keterangan?: string;
+  foto_profil?: string | null;
+  foto_sampul?: string | null;
+}
+
+export interface Database {
+  public: {
+    Tables: {
+      presensi: {
+        Row: {
+          id: string
+          username: string
+          nama: string
+          tanggal: string
+          waktu: string
+          status_presensi: string
         }
+        Insert: {
+          id: string
+          username: string
+          nama: string
+          tanggal: string
+          waktu: string
+          status_presensi: string
         }
-        Views: {}
-        Functions: {}
-        Enums: {}
+        Update: {
+          id?: string
+          username?: string
+          nama?: string
+          tanggal?: string
+          waktu?: string
+          status_presensi?: string
+        }
+      }
     }
-    }
+    Views: {}
+    Functions: {}
+    Enums: {}
+  }
+}
