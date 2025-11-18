cca1d69943e185ea4e9b31201ec4cd298ae0665d
ence <207509775+bayuence@users.noreply.github.com>
2025-11-18 21:26:20 +0700

    Refactor robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,16 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://presensimumi.vercel.app";
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://presensimumi.vercel.app/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/dashboard", "/admin"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
